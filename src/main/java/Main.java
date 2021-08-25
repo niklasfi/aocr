6ac0757382cc59829ecda89e5c3d4a1dfbdb328c
Niklas Fischer <niklas@niklasfi.de>
2021-08-25 18:08:54 +0200

    move file io to FileUtil

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,24 @@
 import de.niklasfi.aocr.APdfRun;
-
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import de.niklasfi.aocr.FileUtil;
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.internal.schedulers.ComputationScheduler;
 
 public class Main {
     public static void main(String[] args) {
         final var inputFilePath = args[0];
         final var outputFilePath = args[1];
 
-        final var run = new APdfRun(inputFilePath, outputFilePath,
-                APdfRun.ImageExtractionStrategy.EXTRACT_LARGEST_IMAGE_FROM_PAGE);
+        final var run = new APdfRun(APdfRun.ImageExtractionStrategy.EXTRACT_LARGEST_IMAGE_FROM_PAGE);
+        final var fileUtil = new FileUtil();
 
-        final var bytes = run.run().blockingGet();
-        try (final var os = new FileOutputStream(outputFilePath)) {
-            os.write(bytes);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("could not open output stream for '%s'".formatted(outputFilePath), e);
-        } catch (IOException e) {
-            throw new RuntimeException("IOException when writing output file", e);
-        }
+        final var scheduler = new ComputationScheduler();
+
+        Flowable.just(inputFilePath)
+                .subscribeOn(scheduler)
+                .observeOn(scheduler)
+                .flatMapSingle(fileUtil::readFile)
+                .flatMapSingle(run::run)
+                .flatMapCompletable(bytes -> fileUtil.writeFile(outputFilePath, bytes))
+                .blockingAwait();
     }
 }
