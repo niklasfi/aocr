c2da642874dfd5ac53eb42c835c0150a49028a6a
Niklas Fischer <niklas@niklasfi.de>
2021-09-04 23:27:00 +0200

    introduce apache commons-cli for command line parsing

--- src/main/java/de/niklasfi/aocr/FileUtil.java
+++ src/main/java/de/niklasfi/aocr/FileUtil.java
@@ -3,13 +3,13 @@ package de.niklasfi.aocr;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Single;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class FileUtil {
     public Single<byte[]> readFile(String path) {
+        if (path.equals("--")) {
+            return Single.fromCallable(() -> readStreamInternal(System.in));
+        }
         return Single.fromCallable(() -> readFileInternal(path));
     }
 
@@ -31,15 +31,37 @@ public class FileUtil {
         return inputBytes;
     }
 
+    private byte[] readStreamInternal(InputStream stream) {
+        final var is = new BufferedInputStream(stream);
+        try {
+            return is.readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException("could not read from input stream", e);
+        }
+    }
+
     public Completable writeFile(String path, byte[] data) {
-        return Completable.fromRunnable(() -> {
-            try (final var os = new FileOutputStream(path)) {
-                os.write(data);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException("could not open output stream for '%s'".formatted(path), e);
-            } catch (IOException e) {
-                throw new RuntimeException("IOException when writing output file", e);
-            }
-        });
+        if (path.equals("--")) {
+            return Completable.fromRunnable(() -> writeStreamInternal(System.out, data));
+        }
+        return Completable.fromRunnable(() -> writeFileInternal(path, data));
+    }
+
+    private void writeStreamInternal(OutputStream os, byte[] data) {
+        try {
+            os.write(data);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not write to output stream", e);
+        }
+    }
+
+    private void writeFileInternal(String path, byte[] data) {
+        try (final var os = new FileOutputStream(path)) {
+            os.write(data);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("could not open output stream for '%s'".formatted(path), e);
+        } catch (IOException e) {
+            throw new RuntimeException("IOException when writing output file", e);
+        }
     }
 }
