bdd4327e65bea6d6f3b99eb659373f8128151dec
Niklas Fischer <niklas.fischer@pwc.com>
2023-04-02 22:47:14 +0200

    stream images instead of using a list
    avoid having every page of the pdf as an image in ram

--- src/main/java/de/niklasfi/aocr/AzurePdfOcr.java
+++ src/main/java/de/niklasfi/aocr/AzurePdfOcr.java
@@ -11,7 +11,6 @@ import org.apache.hc.core5.http.ContentType;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -38,73 +37,61 @@ public class AzurePdfOcr {
     public PdfAndAnnotations ocrGetAnalyzeResults(byte[] inputPdf) {
         final var run = new AzurePdfAnnotator();
 
-        final List<PageContainer<Optional<BufferedImage>>> images;
-        log.trace("parsing input pdf");
         try (
-                final var pdDocIn = PDDocument.load(inputPdf)
-        ) {
-            log.debug("parsed input pdf");
-
-            log.trace("retrieving images from input pdf");
-            images = pdfImageRetriever.getImages(pdDocIn).toList();
-            log.debug("retrieved all images from input pdf");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        log.trace("calling azure read api for annotations");
-        final var annotations = images.stream()
-                .map(pageContainer -> {
-                    final var contImgOpt = pageContainer.data();
-                    if (contImgOpt.isEmpty()) {
-                        log.error("pdfImageRetriever failed to get image for page {}", pageContainer.page());
-                        return new PageContainer<>(pageContainer.page(), Optional.<AnnotatedImage>empty());
-                    }
-                    final var contImg = contImgOpt.get();
-
-                    final byte[] png;
-                    try (final var is = new ByteArrayOutputStream()) {
-                        ImageIO.write(contImg, "png", is);
-                        png = is.toByteArray();
-                    } catch (IOException e) {
-                        log.error("failed to generate png image from BufferedImage");
-                        return new PageContainer<>(
-                                pageContainer.page(),
-                                Optional.of(new AnnotatedImage(contImg, Optional.empty()))
-                        );
-                    }
-                    final ReadResultHeader resultHeader;
-                    try {
-                        final var loc = apiAdapter.waitAnalyze(png, ContentType.IMAGE_PNG, null, Duration.ofSeconds(300));
-                        resultHeader = apiAdapter.waitResult(loc, Duration.ofSeconds(30));
-                    } catch (IOException e) {
-                        log.error("azure api call failed. not adding annotations to page {}", pageContainer.page());
-                        return new PageContainer<>(
-                                pageContainer.page(),
-                                Optional.of(new AnnotatedImage(contImg, Optional.empty()))
-                        );
-                    }
-                    if (resultHeader != null && resultHeader.status() == Status.SUCCEEDED) {
-                        return new PageContainer<>(
-                                pageContainer.page(),
-                                Optional.of(new AnnotatedImage(contImg, Optional.of(resultHeader.analyzeResult())))
-                        );
-                    }
-                    log.error("azure api call did not return results. not adding annotations to page {}", pageContainer.page());
-                    return new PageContainer<>(
-                            pageContainer.page(),
-                            Optional.of(new AnnotatedImage(contImg, Optional.empty()))
-                    );
-                });
-        log.debug("received all annotations");
-
-        log.trace("rendering output pdf");
-        try (
+                final var pdDocIn = PDDocument.load(inputPdf);
                 final var pdDocOut = new PDDocument();
                 final var os = new ByteArrayOutputStream()
         ) {
+            // load font
             final var font = fontLoader.apply(pdDocOut);
-            final var analyzeResults = annotations
+
+            // process pages
+            final var analyzeResults = pdfImageRetriever
+                    // get images
+                    .getImages(pdDocIn)
+                    // process using azure
+                    .map(pageContainer -> {
+                        final var contImgOpt = pageContainer.data();
+                        if (contImgOpt.isEmpty()) {
+                            log.error("pdfImageRetriever failed to get image for page {}", pageContainer.page());
+                            return new PageContainer<>(pageContainer.page(), Optional.<AnnotatedImage>empty());
+                        }
+                        final var contImg = contImgOpt.get();
+
+                        final byte[] png;
+                        try (final var is = new ByteArrayOutputStream()) {
+                            ImageIO.write(contImg, "png", is);
+                            png = is.toByteArray();
+                        } catch (IOException e) {
+                            log.error("failed to generate png image from BufferedImage");
+                            return new PageContainer<>(
+                                    pageContainer.page(),
+                                    Optional.of(new AnnotatedImage(contImg, Optional.empty()))
+                            );
+                        }
+                        final ReadResultHeader resultHeader;
+                        try {
+                            final var loc = apiAdapter.waitAnalyze(png, ContentType.IMAGE_PNG, null, Duration.ofSeconds(300));
+                            resultHeader = apiAdapter.waitResult(loc, Duration.ofSeconds(30));
+                        } catch (IOException e) {
+                            log.error("azure api call failed. not adding annotations to page {}", pageContainer.page());
+                            return new PageContainer<>(
+                                    pageContainer.page(),
+                                    Optional.of(new AnnotatedImage(contImg, Optional.empty()))
+                            );
+                        }
+                        if (resultHeader != null && resultHeader.status() == Status.SUCCEEDED) {
+                            return new PageContainer<>(
+                                    pageContainer.page(),
+                                    Optional.of(new AnnotatedImage(contImg, Optional.of(resultHeader.analyzeResult())))
+                            );
+                        }
+                        log.error("azure api call did not return results. not adding annotations to page {}", pageContainer.page());
+                        return new PageContainer<>(
+                                pageContainer.page(),
+                                Optional.of(new AnnotatedImage(contImg, Optional.empty()))
+                        );
+                    })
                     .map(pageContainer -> {
                         if (pageContainer.data().isPresent()) {
                             run.addPageToDocument(pdDocOut, font, pageContainer.data().get());
